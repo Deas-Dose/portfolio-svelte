dab46ec5a1c45de3b2db6c96f1ea639eac51392b
sohaib farsi <ssohaibfarsi@pm.me>
2023-07-19 01:09:37 +0200

    some changes

---
+++ src/lib/Hero.svelte
@@ -0,0 +1,15 @@
+<script>
+    let src = 'images/me0.jpg';
+</script>
+
+<div class="mt-[5rem] flex-row m-auto text-center text-white font-poppins">
+    <h3 class="text-sm 2xl:text-md">Hello, I'm</h3>
+    <h1 class="text-[2.5rem] 2xl:text-[4rem] mt-5 font-medium">Soheyb FARSI</h1>
+    <h3 class="text-sm mt-[2rem] text-slate-300">Fullstack Developer + Ethical Hacker (Red Team)</h3>
+    <div class="flex mx-auto my-8 gap-4 justify-center">
+        <a class="btn variant-ghost py-4 px-5">CV Not Available</a>
+        <a class="btn variant-filled-tertiary py-4 px-5">Contact Me</a>
+    </div>
+    <img {src} alt="Soheyb FARSI" class="object-cover w-[30rem] h-[30rem] object-cover rounded-t-[15rem]"/>
+</div>
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Hero from '$lib/Hero.svelte';
+</script>
+
+
+<div class="flex justify-center">
+	<Hero />	
+</div>
